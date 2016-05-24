86d3c5c4dad63c53114346087b68400a0e7420d2
99darshan <dsn.adh@gmail.com>
2016-05-24 14:29:06 +0800

    created country module with Country class, defined get_by_name and get_by_region functions

---
+++ restcountry/country.py
@@ -0,0 +1,35 @@
+
+class Country:
+	"""
+
+	"""
+	def __init__(self, a_country):
+
+		"""
+		assigns value to the instance variables
+		:param a_country: dictionary containing all info for a country
+		"""
+		self.name = a_country['name']
+		self.area = a_country['area']
+		self.latlng = a_country['latlng']
+		self.currencies = a_country['currencies']
+		self.subregion = a_country['subregion']
+		self.relevance = a_country['relevance']
+		self.callingCodes = a_country['callingCodes']
+		self.alpha3Code = a_country['alpha3Code']
+		self.timezones = a_country['timezones']
+		self.languages = a_country['languages']
+		self.population = a_country['population']
+		self.capital = a_country['capital']
+		self.borders = a_country['borders']
+		self.demonym = a_country['demonym']
+		self.topLevelDomain = a_country['topLevelDomain']
+		self.altSpellings = a_country['altSpellings']
+		self.translations = a_country['translations']
+		self.gini = a_country['gini']
+		self.alpha2Code = a_country['alpha2Code']
+		self.region = a_country['region']
+		self.nativeName = a_country['nativeName']
+
+	def __str__(self):
+		return self.name
---
+++ restcountry/restcountry.py
@@ -0,0 +1,57 @@
+import requests
+from .country import Country
+
+BASE_API_URL = 'https://restcountries.eu/rest/v1/'
+
+
+def get_response(query_string):
+
+	"""
+	This function makes a request to the API, gets the response, decodes the json
+	as python list.
+	:param query_string:
+	:return: list containing dictionaries of country info
+	"""
+	response = requests.get(BASE_API_URL+query_string)
+
+	if response.status_code == requests.codes.ok:
+		response.encoding('utf-8')
+		countries_list = response.json()
+		return countries_list
+
+	else:
+		return response.raise_for_status()
+
+
+def get_by_name(country_name):
+	"""
+	:param country_name: name of the country to look for
+	:return: a country object, since only one country have same name
+	"""
+
+	# TODO: may be return as a list
+
+	countries_list = get_response("/name/"+country_name)
+
+	# TODO use list comprehension, or just return Country(counries_list[0])
+	# since no two countries have the same name
+
+	for i in range(len(countries_list)):
+		country = Country(countries_list[i])
+
+	return country
+
+
+def get_by_region(region_name):
+
+	"""
+	:param region_name:
+	:return: list of country objects
+	"""
+	countries_list = get_response("/region/"+region_name)
+
+	countries_in_region = []
+	[countries_in_region.append(Country(a_country)) for a_country in countries_list]
+	return countries_in_region
+
+
