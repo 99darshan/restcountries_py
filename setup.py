eda341b854c3b7fc2a5e60b3cee82321c32310f7
99darshan <dsn.adh@gmail.com>
2016-05-27 16:01:47 +0800

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
+
 setup(
-  name = 'restcountries',
-  packages = ['restcountries'], # this must be the same as the name above
-  version = '0.1',
-  description = 'This is a Python wrapper library around the API provided by REST countries http://restcountries.eu',
-  author = '99darshan',
-  author_email = 'dsn.adh@hotmail.com',
-  url = 'https://github.com/99darshan/restcountries-py',
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+	name='restcountries',
+	packages=['restcountries'],  # this must be the same as the name above
+	version='0.1',
+	description='This is a Python wrapper library around the API provided by REST countries http://restcountries.eu',
+	author='99darshan',
+	author_email='dsn.adh@hotmail.com',
+	url='https://github.com/99darshan/restcountries-py',
+	download_url='https://github.com/99darshan/restcountries-py/archive/0.1.tar.gz',
+	keywords=['utility', 'API wrapper', 'restcountries.eu API'],
+	classifiers=[],
 )
