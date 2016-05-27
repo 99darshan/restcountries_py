46b9e95c28743e06dcdf3d8bb689e2f5a9c7ae9b
99darshan <dsn.adh@gmail.com>
2016-05-27 15:18:33 +0800

    added setup.py and requirements.txt

---
+++ restcountries/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'restcountries',
+  packages = ['restcountries'], # this must be the same as the name above
+  version = '0.1',
+  description = 'This is a Python wrapper library around the API provided by REST countries http://restcountries.eu',
+  author = '99darshan',
+  author_email = 'dsn.adh@hotmail.com',
+  url = 'https://github.com/99darshan/restcountries-py',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
