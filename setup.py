45ff6b1b03c28ac8408bfd624cb7065daf9f204d
99darshan <dsn.adh@gmail.com>
2016-05-29 20:30:12 +0800

    edited setup.py for pypi

--- setup.py
+++ setup.py
@@ -20,13 +20,13 @@ from setuptools import setup
 
 setup(
 	name='restcountries_py',
-	version='0.1',
+	version='v0.1',
 	author='Darshan Adhikari',
 	author_email='dsn.adh@hotmail.com',
 	description='A Python wrapper for Restcountries API provided by https://restcountries.eu',
 	keywords=['countries', 'rest countries', 'wrapper', 'database', 'api'],
 	url='https://github.com/99darshan/restcountries_py',
-	download_url='https://github.com/99darshan/restcountries_py/tarball/0.1',
+	download_url='https://github.com/99darshan/restcountries_py/tarball/v0.1',
 	packages=['restcountries_py'],
 	#long_description=long_desc,
 	install_requires=['requests'],
